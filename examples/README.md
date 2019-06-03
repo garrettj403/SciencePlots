a0d8b5ac6be0e15843e73f271ac9b0ae62e4f9ee
John Garrett <john.garrett@astro.ox.ac.uk>
2019-06-03 23:27:00 +0100

    Update example script

--- examples/README.md
+++ examples/README.md
@@ -1,10 +1,4 @@
 Examples
 ========
 
-This directory contains scripts to generate example plots:
-
-1. ``plot-figure.py``: will generate a standard plot using the ``science`` style. This style is suitable for most scientific writing (i.e., journals, theses, etc.).
-
-2. ``plot-subfigure.py``: will generate a sub-figure plot using the ``science`` and ``subfigure`` styles. This style combination is suitable for having two figures side-by-side on a4 paper. See ``subfigure-example.png`` for an example from my thesis.
-
-3. ``plot-square.py``: will generate a square scatter plot using the ``science`` style. Note that the ``figsize`` was set manually in this case.
+This directory contains a script (``plot-examples.py``) to generate the example plots. The results are in ``figures/``.
---
+++ examples/plot-examples.py
@@ -0,0 +1,70 @@
+""" An example of the 'science' theme. """
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+x = np.linspace(0.75, 1.25, 201)
+
+def model(x, p):
+    return x ** (2 * p + 1) / (1 + x ** (2 * p))
+
+with plt.style.context(['science']):
+    fig, ax = plt.subplots()
+    for p in [10, 15, 20, 30, 50, 100]:
+        ax.plot(x, model(x, p), label=p)
+    ax.legend(title='Order')
+    ax.set(xlabel='Voltage (mV)')
+    ax.set(ylabel='Current ($\mu$A)')
+    ax.autoscale(tight=True)
+    fig.savefig('figures/fig1.pdf')
+    fig.savefig('figures/fig1.jpg', dpi=300)
+
+with plt.style.context(['science', 'ieee']):
+    fig, ax = plt.subplots()
+    for p in [10, 15, 20, 30, 50, 100]:
+        ax.plot(x, model(x, p), label=p)
+    ax.legend(title='Order')
+    ax.set(xlabel='Voltage (mV)')
+    ax.set(ylabel='Current ($\mu$A)')
+    ax.autoscale(tight=True)
+    fig.savefig('figures/fig2.pdf')
+    fig.savefig('figures/fig2.jpg', dpi=300)
+
+with plt.style.context(['science', 'scatter']):
+
+    fig, ax = plt.subplots(figsize=(4,4))
+    ax.plot([-2, 2], [-2, 2], 'k--')
+    ax.fill_between([-2, 2], [-2.2, 1.8], [-1.8, 2.2], color='dodgerblue', alpha=0.2, lw=0)
+    for i in range(7):
+        x = np.random.normal(0, 0.5, 10)
+        y = x + np.random.normal(0, 0.2, 10)
+        ax.plot(x, y, label=r"$^\#${}".format(i+1))
+    ax.legend(title='Sample', loc=2)
+    ax.set_xlabel(r"$\log_{10}\left(\frac{L_\mathrm{IR}}{\mathrm{L}_\odot}\right)$")
+    ax.set_ylabel(r"$\log_{10}\left(\frac{L_\mathrm{6.2}}{\mathrm{L}_\odot}\right)$")
+    ax.set_xlim([-2, 2])
+    ax.set_ylim([-2, 2])
+    fig.savefig('figures/fig3.pdf')
+    fig.savefig('figures/fig3.jpg', dpi=300)
+
+with plt.style.context(['science', 'high-vis']):
+    fig, ax = plt.subplots()
+    for p in [10, 15, 20, 30, 50, 100]:
+        ax.plot(x, model(x, p), label=p)
+    ax.legend(title='Order')
+    ax.set(xlabel='Voltage (mV)')
+    ax.set(ylabel='Current ($\mu$A)')
+    ax.autoscale(tight=True)
+    fig.savefig('figures/fig4.pdf')
+    fig.savefig('figures/fig4.jpg', dpi=300)
+
+with plt.style.context(['dark_background', 'science', 'high-vis']):
+    fig, ax = plt.subplots()
+    for p in [10, 15, 20, 30, 50, 100]:
+        ax.plot(x, model(x, p), label=p)
+    ax.legend(title='Order')
+    ax.set(xlabel='Voltage (mV)')
+    ax.set(ylabel='Current ($\mu$A)')
+    ax.autoscale(tight=True)
+    fig.savefig('figures/fig5.pdf')
+    fig.savefig('figures/fig5.jpg', dpi=300)
