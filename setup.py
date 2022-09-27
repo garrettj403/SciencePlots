1a1b13ebc604cf12045f10b4dae297bb354b9bbf
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2022-09-27 16:07:52 +0200

    Update setup.py
    Dunno how I messed up the GH Desktop selections duh
    Now you got everything although commented

--- setup.py
+++ setup.py
@@ -5,15 +5,28 @@ In order to expose .mplstyle files to matplotlib, "import SciencePlots"
 must be called before plt.style.use(...).
 """
 
-import os
+# Commented out code is due to how installation was made.
+# That code is based on a StackOverflow answer:
+# https://stackoverflow.com/questions/31559225/how-to-ship-or-distribute-a-matplotlib-stylesheet
+# It will remain here until a decision is made on if we should uninstall old versions or just ignore the issue.
 
+# import atexit
+# import glob
+import os
+# import shutil
+
+# import matplotlib
 from setuptools import setup
+# from setuptools.command.install import install
 
 
 # def install_styles():
 #     # Find all style files
 #     stylefiles = glob.glob('styles/**/*.mplstyle', recursive=True)
 #     # Find stylelib directory (where the *.mplstyle files go)
+#     mpl_stylelib_dir = os.path.join(matplotlib.get_configdir(), "stylelib")
+#     if not os.path.exists(mpl_stylelib_dir):
+#         os.makedirs(mpl_stylelib_dir)
 #     # Copy files over
 #     print("Installing styles into", mpl_stylelib_dir)
 #     for stylefile in stylefiles:
@@ -23,24 +36,13 @@ from setuptools import setup
 #             os.path.join(mpl_stylelib_dir, os.path.basename(stylefile)))
 
 
-def install_styles():
-    # Find all style files
-    stylefiles = glob.glob('styles/**/*.mplstyle', recursive=True)
-    # Find stylelib directory (where the *.mplstyle files go)
-    mpl_stylelib_dir = os.path.join(matplotlib.get_configdir(), "stylelib")
-    if not os.path.exists(mpl_stylelib_dir):
-        os.makedirs(mpl_stylelib_dir)
-    # Copy files over
-    print("Installing styles into", mpl_stylelib_dir)
-    for stylefile in stylefiles:
-        print(os.path.basename(stylefile))
-        shutil.copy(
 # ('notebook.mplstyle', 'scatter.mplstyle', 'science.mplstyle', 'bright.mplstyle', 'high-contrast.mplstyle', 'high-vis.mplstyle', 'light.mplstyle', 'muted.mplstyle', 'retro.mplstyle', 'std-colors.mplstyle', 'vibrant.mplstyle', 'ieee.mplstyle', 'nature.mplstyle', 'cjk-jp-font.mplstyle', 'cjk-kr-font.mplstyle', 'cjk-sc-font.mplstyle', 'cjk-tc-font.mplstyle', 'grid.mplstyle', 'latex-sans.mplstyle', 'no-latex.mplstyle', 'pgf.mplstyle', 'russian-font.mplstyle', 'sans.mplstyle')
 
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        atexit.register(install_styles)
+# class PostInstallMoveFile(install):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         atexit.register(install_styles)
 
 
 # Get description from README
@@ -69,6 +71,7 @@ setup(
         'Framework :: Matplotlib', 
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3'
+    ],
     keywords=[
         "matplotlib-style-sheets",
         "matplotlib-figures",
