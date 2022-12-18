179470925e466cdf4c8043ded0375345b7e1e093
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2022-12-19 00:29:18 +0100

    Change theme and add some extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,11 @@ release = '2.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +28,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
