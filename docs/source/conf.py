48ba7f7c09512d1b180370ba48a26c16df322f18
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2022-12-19 02:41:36 +0100

    Another extension to live better

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,13 +18,24 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
+    'sphinx.ext.extlinks',
     'sphinx_copybutton'
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+# External links aliases
+repo_base_link = 'https://github.com/garrettj403/SciencePlots/'
+extlinks = {
+    'ghissue': (repo_base_link + 'issues/%s', 'issue %s'),
+    'ghpull': (repo_base_link + 'pull/%s', 'pull request '),
+    'ghwiki': (repo_base_link + 'wiki/%s', 'wiki '),
+    'ghuser': ('https://github.com/%s', '@'),
+    'doi': ('http://doi.org/%s', 'DOI: ')
+}
+# Warn replaceable hardcoded links
+extlinks_detect_hardcoded_links = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
