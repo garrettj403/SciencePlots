619d292409158ad41dfe0fabe7f4ef05b22b6712
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2023-03-08 18:39:01 +0100

    Refactor templates and generator
    A lot of changes went here

--- examples/__examples_generator.py
+++ examples/__examples_generator.py
@@ -3,17 +3,11 @@
 from pathlib import Path
 
 import jinja2
-import scienceplots  # NOQA: F401
 
 # Paths we will need
 THIS_FILEDIR = Path(__file__).parent.resolve()
 STYLES_PATH = THIS_FILEDIR.joinpath('../scienceplots/styles')
-TEMPLATE_PLOT_FILE = THIS_FILEDIR.joinpath('template_plot.py.jinja2')
-TEMPLATE_PLOT_SCATTER_FILE = \
-    THIS_FILEDIR.joinpath('template_plot_scatter.py.jinja2')
-TEMPLATE_PLOT_LANGUAGE_FILE = \
-    THIS_FILEDIR.joinpath('template_plot_languages.py.jinja2')
-TEMPLATE_README_FILE = THIS_FILEDIR.joinpath('template_README.rst.jinja2')
+TEMPLATES_PATH = THIS_FILEDIR.joinpath('templates')
 
 
 def get_styles_names_list_in(dir):
@@ -39,52 +33,48 @@ STYLES = {
 OUTPUT_FOLDERS = {k: '_'.join([k, 'styles']).title()
                   for k in STYLES.keys()}
 
-# Initialize examples subfolders and readmes
-# Read README template
-with open(TEMPLATE_README_FILE, mode='r') as f:
-    readme_template = jinja2.Template(f.read(), keep_trailing_newline=True)
-
-for folder_name in OUTPUT_FOLDERS.values():
-    # Create subfolder
-    output_folder = THIS_FILEDIR.joinpath(folder_name)
-    if not output_folder.exists():
-        output_folder.mkdir()
-    # Create README.rst from template
-    readme_path = output_folder.joinpath('README.rst')
-    if not readme_path.exists():
-        # Use the folder name as title for readme, so substitute underscores
-        # by spaces
-        readme = readme_template.render(title=folder_name.replace('_', ' '))
-        with open(readme_path, mode='w') as readme_file:
-            readme_file.write(readme)
-
-# Create plot_* examples files
-
 # This is the relevant generator of examples section
 # VARIABLES REFERENCE:
-#  * STYLES[key]: return set of styles (e.g. {'science', 'notebook', ...})
-#    - key: 'BASE', 'COLOR', 'JOURNALS', 'LANGUAGE', 'MISCELLANEOUS'
-#    - Use 'group' to select one key of above, in EXAMPLE TEMPLATE GENERATOR
-#    - Use 'ignore' to leave out any of the styles
+# * OUTPUT_FOLDERS: dict of names given to each directory, for each group of
+#     styles.
+# * STYLES[key]: return set of styles (e.g. {'science', 'notebook', ...})
+#     - key: 'BASE', 'COLOR', 'JOURNALS', 'LANGUAGE', 'MISCELLANEOUS'
+#     - Use 'group' to select one key of above, in EXAMPLE TEMPLATE GENERATOR
+#     - Use 'ignore' to leave out any of the styles
 #
 # TEMPLATES REFERENCE:
-# * template_plot.py.jinja2 notes:
-#     Input:
-#        * styles: list-like (just what you would pass to plt.style.use(...))
-#            Template will format the string as needed
-# * template_plot_scatter.py.jinja2 notes:
-#     Input:
-#        * styles: list-like (same as in template_plot.py.jinja2)
-# * template_README.rst.jinja2 notes:
+# * base_example.py.jinja2
+#     Main basic example, extended by other examples templates
+#       Input:
+#         * styles: list-like (just what you would pass to plt.style.use(...))
+#             Template will format the string as needed
+#       Blocks:
+#         * description_extended: defaults to nothing.
+#             Used to provide specific descriptions.
+#         * lang_texts: has following self-explanatory variables:
+#               - lang_pparam: dict of str with keys xlabel and ylabel
+#               - lang_legend: str
+#             Default values for them can be looked up in source file.
+#             Can also be used to insert constant code, like np.random.seed()
+#         * body: defaults to general example code.
+# * scatter_example.py.jinja2
+#     Substitutes body code in order to generate legacy fig3.jpg
+#     under old 'examples/figures'
+#       Defines blocks:
+#         * lang_texts: labels are formulae and equations
+#         * body: mentioned example code
+# * language_example.py.jinja2
+#     General example template for languages. Allows broader input of labels
+#     and titles.
+#       Input:
+#           - styles: list-like (same as in base_example.py.jinja2)
+#           - legend_title: raw string, title for the legend
+#           - xlabel: raw string, label for x-axis
+#           - ylabel: raw string, label for y-axis
+# * README.rst.jinja2
 #     Creates title for subsection in Gallery.
 #     Input:
 #       - title: string
-# * template_plot_languages.py.jinja2 notes:
-#     Input:
-#       - styles: list-like (same as in template_plot.py.jinja2)
-#       - legend_title: string, title for the legend
-#       - xlabel: string, label for x-axis
-#       - ylabel: string, label for y-axis
 #
 # EXAMPLE GENERATOR TEMPLATE:
 # group = 'BASE'
@@ -96,27 +86,44 @@ for folder_name in OUTPUT_FOLDERS.values():
 #         continue
 #     current_example_path = output_folder.joinpath('plot_' + style + '.py')
 #     # Convert style to list (input to template)
-#     example_styles = [style]
+#     example_styles = [style]  # main style last to preserve precedence
 #     example_text = plot_template.render(styles=example_styles)
 #     with current_example_path.open('w') as example:
 #         example.write(example_text)
-# print(f'Group {group}: created {n_group_examples} examples')
+# # Report number of created examples
+# print(f'Group {group}: {n_group_examples} / {len(STYLES[group])})
 # n_total_examples += n_group_examples
 # END OF TEMPLATE
 # See BASE Styles code for a complete pattern including ignored styles
 
-# Read example plot_* template
-with open(TEMPLATE_PLOT_FILE, mode='r') as f:
-    plot_template = jinja2.Template(f.read(), keep_trailing_newline=True)
+template_env = \
+    jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATES_PATH),
+                       keep_trailing_newline=True)
 
-# Read example plot_scatter* template
-with open(TEMPLATE_PLOT_SCATTER_FILE, mode='r') as f:
-    scatter_template = jinja2.Template(f.read(), keep_trailing_newline=True)
+# Create examples subfolders and initialize READMEs
+# Read README template
 
-# Read example Language_Styles/plot_* template
-with open(TEMPLATE_PLOT_LANGUAGE_FILE, mode='r') as f:
-    language_template = jinja2.Template(f.read(), keep_trailing_newline=True)
-LANG_PARAMS = {  # Language dicts to generate examples
+readme_template = template_env.get_template('README.rst.jinja2')
+for folder_name in OUTPUT_FOLDERS.values():
+    # Create subfolder
+    output_folder = THIS_FILEDIR.joinpath(folder_name)
+    if not output_folder.exists():
+        output_folder.mkdir()
+    # Create README.rst from template
+    readme_path = output_folder.joinpath('README.rst')
+    if not readme_path.exists():
+        # Use the folder name as title for readme,
+        # so we substitute its underscores by spaces
+        readme = readme_template.render(title=folder_name.replace('_', ' '))
+        with open(readme_path, mode='w') as readme_file:
+            readme_file.write(readme)
+
+# Read templates
+plot_template = template_env.get_template('base_example.py.jinja2')
+scatter_template = template_env.get_template('scatter_example.py.jinja2')
+language_template = template_env.get_template('language_example.py.jinja2')
+
+LANG_PARAMS = {  # Dicts to generate language examples
     'cjk-tc-font': {'legend_title': 'Order',
                     'xlabel': r'電壓 (mV)',
                     'ylabel': r'電流 ($\mu$A)'},
@@ -137,12 +144,14 @@ LANG_PARAMS = {  # Language dicts to generate examples
                      'ylabel': r'Mevcut Güç/Akım ($\mu$A)'},
 }
 
-n_total_examples = 0
+# Report examples vs styles created
+n_total_examples = 0  # Number of global examples created
+print('Group\tCreated examples / Styles in group')
 
 # BASE Styles
 group = 'BASE'
 ignore = {'scatter'}
-n_group_examples = 0
+n_group_examples = 0  # Number of examples created for group
 output_folder = THIS_FILEDIR.joinpath(OUTPUT_FOLDERS[group])
 for style in STYLES[group]:
     if style in ignore:
@@ -157,12 +166,12 @@ for style in STYLES[group]:
 # Use same output folder
 for style in ignore:
     current_example_path = output_folder.joinpath('plot_' + style + '.py')
-    example_styles = [style] + ['science']
+    example_styles = ['science'] + [style]
     example_text = scatter_template.render(styles=example_styles)
     with current_example_path.open('w', encoding='UTF-8') as example:
         example.write(example_text)
         n_group_examples += 1
-print(f'Group {group}: created {n_group_examples} examples')
+print(f'Group {group}: {n_group_examples} / {len(STYLES[group])}')
 n_total_examples += n_group_examples
 
 # COLOR Styles
@@ -174,12 +183,12 @@ for style in STYLES[group]:
     if style in ignore:
         continue
     current_example_path = output_folder.joinpath('plot_' + style + '.py')
-    example_styles = [style] + ['science']
+    example_styles = ['science'] + [style]
     example_text = plot_template.render(styles=example_styles)
     with current_example_path.open('w', encoding='UTF-8') as example:
         example.write(example_text)
         n_group_examples += 1
-print(f'Group {group}: created {n_group_examples} examples')
+print(f'Group {group}: {n_group_examples} / {len(STYLES[group])}')
 n_total_examples += n_group_examples
 
 # JOURNALS Styles
@@ -191,12 +200,12 @@ for style in STYLES[group]:
     if style in ignore:
         continue
     current_example_path = output_folder.joinpath('plot_' + style + '.py')
-    example_styles = [style] + ['science']
+    example_styles = ['science'] + [style]
     example_text = plot_template.render(styles=example_styles)
     with current_example_path.open('w', encoding='UTF-8') as example:
         example.write(example_text)
         n_group_examples += 1
-print(f'Group {group}: created {n_group_examples} examples')
+print(f'Group {group}: {n_group_examples} / {len(STYLES[group])}')
 n_total_examples += n_group_examples
 
 # LANGUAGE Styles
@@ -208,14 +217,16 @@ for style in STYLES[group]:
     if style in ignore:
         continue
     current_example_path = output_folder.joinpath('plot_' + style + '.py')
-    example_styles = [style] + ['science']
+    if style in {'turkish-font'}:
+        no_latex_optional = True
+    example_styles = ['science', 'no-latex'] + [style]
     example_text = language_template.render(styles=example_styles,
                                             # Unpack language strings
                                             **LANG_PARAMS[style])
     with current_example_path.open('w', encoding='UTF-8') as example:
         example.write(example_text)
         n_group_examples += 1
-print(f'Group {group}: created {n_group_examples} examples')
+print(f'Group {group}: {n_group_examples} / {len(STYLES[group])}')
 n_total_examples += n_group_examples
 
 # MISCELLANEOUS Styles
@@ -227,12 +238,12 @@ for style in STYLES[group]:
     if style in ignore:
         continue
     current_example_path = output_folder.joinpath('plot_' + style + '.py')
-    example_styles = [style] + ['science']
+    example_styles = ['science'] + [style]
     example_text = plot_template.render(styles=example_styles)
     with current_example_path.open('w') as example:
         example.write(example_text)
         n_group_examples += 1
-print(f'Group {group}: created {n_group_examples} examples')
+print(f'Group {group}: {n_group_examples} / {len(STYLES[group])}')
 n_total_examples += n_group_examples
 
 n_of_styles = sum(len(STYLES[k]) for k in STYLES.keys())
