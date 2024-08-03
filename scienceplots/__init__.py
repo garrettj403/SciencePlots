e7adddce61e5d9e68fb2e9bdd6f2971cb951d576
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-08-03 21:32:16 +0200

    Add tests
    commit 5e0857a187c006f8e438afcc70e61ba6b994ab5d
    Author: echedey-ls <80125792+echedey-ls@users.noreply.github.com>
    Date:   Sat Aug 3 21:31:46 2024 +0200
    
        Create pytest.yaml
    
    commit a656350bcf3d264f8f9cb839485b57fed055de03
    Author: echedey-ls <80125792+echedey-ls@users.noreply.github.com>
    Date:   Sat Aug 3 20:58:26 2024 +0200
    
        Configure ruff formatting
    
    commit 0749af926de47280663d9f7ff295cb6723184408
    Author: echedey-ls <80125792+echedey-ls@users.noreply.github.com>
    Date:   Sat Aug 3 20:58:18 2024 +0200
    
        Remove warning
    
    commit 2af9fa514561fac6e881fcf2bdd004593285ac3c
    Author: echedey-ls <80125792+echedey-ls@users.noreply.github.com>
    Date:   Sat Aug 3 20:47:00 2024 +0200
    
        Format with ruff
    
    commit 98d0f6af23621eeaf30b1aad86597c79d48bfd31
    Author: echedey-ls <80125792+echedey-ls@users.noreply.github.com>
    Date:   Sat Aug 3 20:40:27 2024 +0200
    
        Add tests and port setup.py to pyproject.toml

--- scienceplots/__init__.py
+++ scienceplots/__init__.py
@@ -2,16 +2,14 @@ import os  # pathlib.Path.walk not available in Python <3.12
 import matplotlib.pyplot as plt
 
 import scienceplots
+from .styles_discovery import read_styles_in_folders
 
 # register the bundled stylesheets in the matplotlib style library
 scienceplots_path = scienceplots.__path__[0]
-styles_path = os.path.join(scienceplots_path, 'styles')
+styles_path = os.path.join(scienceplots_path, "styles")
 
 # Reads styles in /styles folder and all subfolders
-stylesheets = {}  # plt.style.library is a dictionary
-for folder, _, _ in os.walk(styles_path):
-    new_stylesheets = plt.style.core.read_style_directory(folder)
-    stylesheets.update(new_stylesheets)
+stylesheets = read_styles_in_folders(styles_path)
 
 # Update dictionary of styles - plt.style.library
 plt.style.core.update_nested_dict(plt.style.library, stylesheets)
