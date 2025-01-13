1d5900209d565039e3aee950f92ed13ee9665401
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2025-01-13 22:27:13 +0000

    ruff linting

--- scienceplots/tests/test_scienceplots.py
+++ scienceplots/tests/test_scienceplots.py
@@ -1,12 +1,13 @@
-"""
-Test suite of SciencePlots
+"""Test suite of SciencePlots
 """
 
 import matplotlib.pyplot as plt
 
 
 def test_matplotlib_required_api_existence():
-    """Check if all necessary functions and attributes are available in matplotlib."""
+    """Check if all functions and attributes used by scienceplots are available
+    in matplotlib.
+    """
     assert hasattr(plt.style, "core")
     assert hasattr(plt.style.core, "read_style_directory")
     assert hasattr(plt.style.core, "update_nested_dict")
@@ -19,16 +20,16 @@ def test_styles_existence(styles_in_scienceplots_per_folder):
     for folder, styles in styles_in_scienceplots_per_folder.items():
         assert len(styles) > 0, f"No styles found in {folder}."
         for style in styles:
-            assert (  # both in list of names and the library they are retrieved from
+            assert (  # both in list of names and the library they are retrieved from hello
                 style in plt.style.available and style in plt.style.library
             ), f"'{style}' not in available styles. Style in folder {folder}."
 
 
 def test_usage_of_each_style(
-    xy_example_values, styles_in_scienceplots_per_folder, tmp_path
+    xy_example_values, styles_in_scienceplots_per_folder, tmp_path,
 ):
     """Tests if the styles are correctly formatted and can be applied to a plot."""
-    pparam = dict(xlabel="Voltage (mV)", ylabel=r"Current ($\mu$A)")
+    pparam = {"xlabel": "Voltage (mV)", "ylabel": r"Current ($\mu$A)"}
     x, ys, p = xy_example_values
     for folder, styles in styles_in_scienceplots_per_folder.items():
         for style in styles:
